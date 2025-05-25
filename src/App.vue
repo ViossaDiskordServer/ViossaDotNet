ccadb3f1603cc32bab7be8c8a9d4a40fdf46259b
jezzahehn <jezza.hehn@wikitribune.com>
2025-05-24 22:22:11 -0400

    chore: comment cleanup

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,6 @@
 </template>
 
 <script setup lang="ts">
-// import HomeSectionWrapper from './components/molecules/HomeSectionWrapper.vue'
 import './assets/style.scss'
 import 'bulma/css/bulma.css'
-// import { useI18n } from 'vue-i18n'
-// import type { MessageSchema } from './i18n/types'
-
-// const { t, tm } = useI18n()
-
-// console.log(sectionList.value)
 </script>
