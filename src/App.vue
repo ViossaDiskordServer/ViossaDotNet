8c0c9e01696cb4d775f3699f9ee1df6fb20bc779
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-06-07 18:49:14 +0330

    made header into it's own file

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,13 @@
 <template>
   <div class="min-h-screen flex flex-col "><!-- Main application wrapper -->
-    <header
-    style="backdrop-filter: blur(8px); background-color: color-mix(in srgb, var(--bulma-navbar-background-color) 80%, transparent);"
-    class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-
-      <div class="navbar-brand">
-        <RouterLink class="navbar-item has-text-weight-bold p-0" to="/" >
-          <img src="@/assets/ViossaFlagRect.svg" alt="Viossa flag" style="height: var(--bulma-navbar-height); max-height: none;" />
-        </RouterLink> 
-
-        <div class="navbar-item">
-          <button 
-            type="button"
-            @click="toggleBurger()"
-            :class="`button is-link is-hoverable is-hidden-desktop ${burgerOpen ? 'is-active' : ''}`" aria-label="menu"
-            :aria-expanded="`${burgerOpen ? 'true' : 'false'}`">
-            <span class="bx bx-burger"></span>
-          </button>
-        </div>
-      </div>
-      
-      <div :class="`navbar-menu ${burgerOpen ? 'is-active' : ''}`">
-        <div class="navbar-start">
-          <RouterLink class="navbar-item" to="/">What is Viossa?</RouterLink>
-          <RouterLink class="navbar-item" to="/resources">Resources</RouterLink>
-        </div>
-      </div>
-
-    </header style="backdrop-filter: blur(8px); background-color: oklab(80% var(--bulma-navbar-background-color) transparent);">
+    <Header />
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
 import './assets/style.scss'
+import Header from './components/organisms/Header.vue'
 import { ref, type Ref } from 'vue'
 
 const burgerOpen: Ref<Boolean> = ref<Boolean>(false);
---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header
+    style="backdrop-filter: blur(8px); background-color: color-mix(in srgb, var(--bulma-navbar-background-color) 80%, transparent);"
+    class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+
+      <div class="navbar-brand">
+        <RouterLink class="navbar-item has-text-weight-bold p-0" to="/" >
+          <img src="@/assets/ViossaFlagRect.svg" alt="Viossa flag" style="height: var(--bulma-navbar-height); max-height: none;" />
+        </RouterLink> 
+
+        <div class="navbar-item">
+          <button 
+            type="button"
+            @click="toggleBurger()"
+            :class="`button is-link is-hoverable is-hidden-desktop ${burgerOpen ? 'is-active' : ''}`" aria-label="menu"
+            :aria-expanded="`${burgerOpen ? 'true' : 'false'}`">
+            <span class="bx bx-burger"></span>
+          </button>
+        </div>
+      </div>
+      
+      <div :class="`navbar-menu ${burgerOpen ? 'is-active' : ''}`">
+        <div class="navbar-start">
+          <RouterLink class="navbar-item" to="/">What is Viossa?</RouterLink>
+          <RouterLink class="navbar-item" to="/resources">Resources</RouterLink>
+        </div>
+      </div>
+
+    </header style="backdrop-filter: blur(8px); background-color: oklab(80% var(--bulma-navbar-background-color) transparent);">
+</template>
