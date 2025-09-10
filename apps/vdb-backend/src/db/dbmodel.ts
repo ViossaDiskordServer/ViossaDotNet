457f5dce910226d15de5c362646013a0c5192c9c
Sheldon Cooper <nikoraito@gmail.com>
2025-09-10 12:24:05 -0400

    wip: add db classes; can't load SQLite driver.

---
+++ apps/vdb-backend/src/db/dbmodel.ts
@@ -0,0 +1,124 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  PrimaryColumn,
+  BaseEntity,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  ManyToMany,
+  JoinTable,
+  ColumnType
+} from "typeorm";
+
+import "reflect-metadata"
+
+@Entity()
+export class Lemma extends BaseEntity {
+  @PrimaryColumn({type: 'string'})
+  lemma_name: string;
+
+  @OneToMany(() => WordForm, (word_form) => word_form.lemma)
+  word_forms: WordForm[];
+
+  @OneToMany(() => Example, (example) => example.lemma)
+  examples: Example[];
+
+  @OneToMany(() => Definition, (definition) => definition.lemma)
+  definitions: Definition[];
+
+  @OneToMany(() => Comment, (comment) => comment.lemma)
+  comments: Comment[];
+
+  @OneToMany(() => Media, (media) => media.lemma)
+  media: Media[];
+
+  @ManyToMany(() => PartOfSpeech)
+  @JoinTable()
+  parts_of_speech: PartOfSpeech[];
+
+}
+
+@Entity()
+export class Lect extends BaseEntity {
+  @PrimaryColumn({type: 'string'})
+  name: string;
+
+  @OneToMany(() => WordForm, (w) => w.lect)
+  word_forms: WordForm[];
+
+}
+
+@Entity()
+export class WordForm extends BaseEntity {
+  @PrimaryColumn({type: 'number'})
+  word_form_id: number
+  
+  @Column({type: 'string'})
+  word_form: string;
+
+  @ManyToOne(() => Lemma, (lemma) => lemma.word_forms)
+  lemma: Lemma;
+
+  @ManyToOne(() => Lect, (lect) => lect.word_forms)
+  lect: Lect;
+}
+
+@Entity()
+export class Example extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  example_id: number;
+
+  @Column({ nullable: false, type: 'string' })
+  example_text: string;
+
+  @ManyToOne(() => Lemma, (lemma) => lemma.examples)
+  lemma: Lemma; 
+}
+
+@Entity()
+export class Media extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  media_id: number;
+
+  @Column({ nullable: false, type: 'string'})
+  media_url: string;
+
+  @ManyToOne(() => Lemma, (lemma) => lemma.media)
+  lemma: Lemma;
+}
+
+@Entity()
+export class Definition extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  definition_id: number;
+
+  @Column({ nullable: false, type: 'string' })
+  definition_text: string;
+
+  @ManyToOne(() => Lemma, (lemma) => lemma.definitions)
+  lemma: Lemma;
+}
+
+@Entity()
+export class Comment extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  comment_id: number;
+
+  @Column({ nullable: false, type: 'string' })
+  comment_text: string;
+
+  @ManyToOne(() => Lemma, (lemma) => lemma.comments)
+  lemma: Lemma;
+}
+
+@Entity()
+export class PartOfSpeech extends BaseEntity {
+  @PrimaryColumn({type: 'string'})
+  long_form: string;
+
+  @Column({ nullable: false, unique: true, type: 'string' })
+  short_form: string;
+}
+
