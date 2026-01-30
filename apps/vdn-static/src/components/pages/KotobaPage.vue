87fc7a951ae3e92043ac26764d11d6a0a20e1da2
Nikomiko <nikoraito@gmail.com>
2026-01-30 01:01:42 -0500

    fix basic query

--- apps/vdn-static/src/components/pages/KotobaPage.vue
+++ apps/vdn-static/src/components/pages/KotobaPage.vue
@@ -5,13 +5,23 @@
 		</section>
 
 		<section class="section container">
-			<p class="notification">
-				⚠ Under construction :3 ⚠
-			</p>
+			<div class="notification is-info block">
+				<p>To searcn tropos-agnostically, enter a term below.</p>
+			</div>
+
+			<div class="block is-flex is-flex-direction-row is-gap-2">
+				<input class="input" type="text"></input>
+			</div>
+
+			
+			<div class="container">
+				
+			</div>
+			
 		</section>
 	</div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 
 </script>
