13e98087c95e5dbbf414d3375865330bc9f695fb
nikomiko <nikoraito@gmail.com>
2026-02-08 08:15:23 -0500

    fix: scripts
    remove 'or' clauses as they seem to cause the job to be labeled as success even when it's a failure

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,16 @@ STATIC_DIR=$2
 BACKEND_DIR=$3
 
 pwd
-npm i -g pnpm && pnpm setup || { echo "Can't npm 😓 - code $?"; exit $?; } 
-which pm2 || pnpm i pm2 -g  || { echo "😓 PM2 missing and can't install - run 'pnpm i pm2 -g'  code $?"; exit $?; } 
-cd $SOURCE_DIR || { echo "😓 can't cd - code $?"; exit $?; } 
-pnpm i || { echo "😓 can't pnpm i - $?"; exit $?; } 
-npx turbo build || { echo "😓 build failed - code $?"; exit $?; } 
+npm i -g pnpm && pnpm setup 
+which pm2 || pnpm i pm2 -g
+cd $SOURCE_DIR
+pnpm i
+npx turbo build
 
-mkdir -p $STATIC_DIR || { echo "😓 Can't mkdir $STATIC_DIR  - code $?"; exit $?; } 
-mkdir -p $BACKEND_DIR || { echo "😓 Can't mkdir $BACKEND_DIR - code $?"; exit $?; } 
-cp -r $SOURCE_DIR/apps/vdn-static/dist/* "$STATIC_DIR" || { echo "😓 Can't copy static dist - code $?"; exit $?; } 
-cp -r $SOURCE_DIR/apps/vdb-backend/dist/* "$BACKEND_DIR" || { echo "😓 Can't copy backend dist - code $?"; exit $?; } 
-cd $BACKEND_DIR || { echo "😓 can't cd - code $?"; exit $?; } 
+mkdir -p $STATIC_DIR
+mkdir -p $BACKEND_DIR
+cp -r $SOURCE_DIR/apps/vdn-static/dist/* "$STATIC_DIR"
+cp -r $SOURCE_DIR/apps/vdb-backend/dist/* "$BACKEND_DIR"
+cd $BACKEND_DIR
 
 pm2 start .
