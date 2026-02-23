c69d097a2b90b78dbb06329cdd953b6fe1c2db26
TheDragonOfFlame <78053172+TheDragonOfFlame@users.noreply.github.com>
2026-02-23 08:50:57 -0500

    Update greeting.ts

--- apps/vdn-static/src/i18n/greeting.ts
+++ apps/vdn-static/src/i18n/greeting.ts
@@ -20,4 +20,58 @@ export const GREETINGS = [
 		author: "Tetro",
 		lang: "wodox",
 	},
+	{
+		title: "VIOSSA.NET VR̄ATULAŢAJO",
+		subtitle: "Hažilɛ̄ti na viɔssalɛɾa! Viɔssa lɛstɛvr̄ā ɡlɔssa﹐tɛndɔţa!",
+		author: "Rju",
+		lang: "viossa",
+	},
+	{
+		title: "bratsatulla na viossa.net made!",
+		subtitle: "Furalehti vilanta",
+		author: "Niko",
+		lang: "viossa",
+	},
+	{
+		title: "Bratula na viossa.net!",
+		subtitle: "Davi lera vjosa medrio!",
+		author: "2o3ka",
+		lang: "viossa",
+	},
+	{
+		title: "ברא־תולהצה viossa.net מדא!",
+		subtitle: "דבֿי לרה איו הנסו שתוף צויתה נא בֿיוסה",
+		author: "Visa Chin",
+		lang: "viossa",
+	},
+	{
+		title: "Bratullatsa viossa.net made!",
+		subtitle: "Leratsa Viossa au letstehal sztof andra derna",
+		author: "Visa Chin",
+		lang: "viossa",
+	},
+	{
+		title: "BRATULA VIOSSA.NET MADE",
+		subtitle: "Hadжilehti pęr ʋjosalera! Nintendotsa",
+		author: "Kurokot",
+		lang: "viossa",
+	},
+	{
+		title: "бrατuλα αδ viossa.net mαᴅε!!",
+		subtitle: "xαᵹιλε̃τι v́ ʋιossα λεrαᴅѥτ! nιnτεnᴅocα nαruγα!",
+		author: "Orenge",
+		lang: "viossa",
+	},
+	{
+		title: "Bratulla na viossa.net made!",
+		subtitle: "Hazsilehti fu viossaklani—yuenttsa na her yo!",
+		author: "Zsiyo",
+		lang: "viossa",
+	},
+	{
+		title: "Alú ri viossa.net-ssa!",
+		subtitle: "Ya vir atuarpik Viossáha druzsai-mais!",
+		author: "Zsiyo",
+		lang: "minemiaha",
+	},
 ] as const satisfies Greeting[];
