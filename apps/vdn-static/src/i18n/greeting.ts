ce6812bf32fa754993e12a01563e967acc2d6262
AmberWing <63614345+lunarythia@users.noreply.github.com>
2026-04-01 10:45:49 -0700

    greetings.ts: po'antaandan troposnui

--- apps/vdn-static/src/i18n/greeting.ts
+++ apps/vdn-static/src/i18n/greeting.ts
@@ -98,4 +98,10 @@ export const GREETINGS = [
 		author: "Luna",
 		lang: "viossa",
 	},
+	{
+		title: "Braatullaca viossa.net:an!",
+		subtitle: "Afto Zeplas viossja je ! Braaleraca Viossa au nintendoca !",
+		author: "Lunakot",
+		lang: "viossa",
+	},
 ] as const satisfies Greeting[];
